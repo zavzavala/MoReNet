b3fbd04684565507734b7fc6ac359f6d316f910c
zavzavala <zavzavala76@gmail.com>
2024-01-30 15:23:41 +0630

    versao 2024

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $this->call(MesSeeder::class);
+        $this->call(ServicosSeeder::class);
+        $this->call(tipo_empresa::class);
+        $this->call(TypeSeeder::class);
+        $this->call(UserSeeder::class);
+        
     }
 }
